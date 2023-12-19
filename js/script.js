24ca3c3bb308a97655fdf034cefcfde17a215beb
CBAL <fer123arriaga@gmail.com>
2023-12-18 22:21:54 -0600

    testing

--- js/script.js
+++ js/script.js
@@ -24,11 +24,8 @@ const currentShowing = () => {
     }
 };
 
-let i = 0
-
-while(i < 10) {
+for(let i = 0; i < 10; i++) {
     console.log(i);
-    i++;
 }
 
 // let showingRightNow = currentShowing();
